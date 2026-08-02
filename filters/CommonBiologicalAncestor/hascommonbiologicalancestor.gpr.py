c47450e3cacd42cee6d23eb34639d48e1a5f117b
HN1962 <hndesign@msn.com>
2026-08-02 18:55:22 +0200

    Kan nu bruge på versioner før 6.0

--- filters/CommonBiologicalAncestor/hascommonbiologicalancestor.gpr.py
+++ filters/CommonBiologicalAncestor/hascommonbiologicalancestor.gpr.py
@@ -1,27 +1,47 @@
 #
-# Gramps Plugin Registration (GPR) for the "Common biological ancestor" rule
+# Gramps Plugin Registration (GPR) for the "common biological ancestor" rule
+# Gramps - a GTK+/GNOME based genealogy program
 #
-# NOTE: set gramps_target_version to YOUR Gramps version (major.minor),
-# e.g. "5.2" or "6.0". You can find it under Help -> About.
+# Copyright (C) 2026      Henning
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+
+# NOTE:
 # "register", "RULE", "STABLE" AND "_" are provided automatically by the
 # Gramps plugin loader -- do not import or reassign them. When a locale/
 # folder is present next to this file, "_" is this addon's own translator.
 
-register(
-    RULE,
-    id="HasCommonBiologicalAncestorWith",
-    name=_("People with a common biological ancestor with <person>"),
-    description=_(
-        "Matches people who share a biological ancestor; only birth "
-        "relationships are followed, so purely adopted and foster children "
-        "are excluded."
-    ),
-    version="1.0.0",
-    authors=["Henning"],
-    authors_email=[""],
-    gramps_target_version="6.0",
-    status=STABLE,
-    fname="hascommonbiologicalancestor.py",
-    ruleclass="HasCommonBiologicalAncestorWith",  # must match the class name
-    namespace="Person",                           # rule in the Person category
-)
+from gramps.version import major_version, VERSION_TUPLE
+
+if VERSION_TUPLE >= (5, 2, 0):
+    register(
+        RULE,
+        id="HasCommonBiologicalAncestorWith",
+        name=_("People with a common biological ancestor with <person>"),
+        description=_(
+            "Matches people who share a common ancestor with a specified "
+            "person, following biological parent-child links only "
+            "(adoptive, foster and step relationships are excluded)."
+        ),
+        version="1.1.0",
+        authors=["Henning"],
+        authors_email=["https://myown-project.dk"],
+        gramps_target_version=major_version,
+        status=STABLE,
+        fname="hascommonbiologicalancestor.py",  # must match your .py file
+        ruleclass="HasCommonBiologicalAncestorWith",  # must match the class name
+        namespace="Person",                           # rule in the Person category
+        help_url="https://myown-project.dk/tools/gramps-filter-rules/common-biological-ancestor",
+    )
--- filters/NonBirthChildRelation/hasnonbirthchildrelation.gpr.py
+++ filters/NonBirthChildRelation/hasnonbirthchildrelation.gpr.py
@@ -1,27 +1,48 @@
 #
 # Gramps Plugin Registration (GPR) for the "non-birth child" rule
+# Gramps - a GTK+/GNOME based genealogy program
 #
-# NOTE: set gramps_target_version to YOUR Gramps version (major.minor),
-# e.g. "5.2" or "6.0". You can find it under Help -> About.
+# Copyright (C) 2026      Henning
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+# NOTE: 
 # "register", "RULE", "STABLE" AND "_" are provided automatically by the
 # Gramps plugin loader -- do not import or reassign them. When a locale/
 # folder is present next to this file, "_" is this addon's own translator.
 
-register(
-    RULE,
-    id="HasNonBirthChildRelation",
-    name=_("People recorded as a non-birth child"),
-    description=_(
-        "Matches people recorded as a child with a non-birth relationship "
-        "to a parent (adopted, foster, step, etc.). Helps review that child "
-        "relationship types are set correctly."
-    ),
-    version="1.1.0",
-    authors=["Henning"],
-    authors_email=[""],
-    gramps_target_version="6.0",
-    status=STABLE,
-    fname="hasnonbirthchildrelation.py",
-    ruleclass="HasNonBirthChildRelation",  # must match the class name
-    namespace="Person",                    # rule in the Person category
-)
+from gramps.version import major_version, VERSION_TUPLE
+
+if VERSION_TUPLE >= (5, 2, 0):
+    register(
+        RULE,
+        id="HasNonBirthChildRelation",
+        name=_("People recorded as a non-birth child"),
+        description=_(
+            "Matches people recorded as a child with a non-birth relationship "
+            "to a parent (adopted, foster, step, etc.). Helps review that child "
+            "relationship types are set correctly."
+        ),
+        version="1.1.0",
+        authors=["Henning"],
+        authors_email=["https://myown-project.dk"],
+        gramps_target_version=major_version,
+        status=STABLE,
+        fname="hasnonbirthchildrelation.py",
+        ruleclass="HasNonBirthChildRelation",  # must match the class name
+        namespace="Person",                    # rule in the Person category
+        help_url= "https://myown-project.dk/tools/gramps-filter-rules/non-birth-child-relation",
+    )
